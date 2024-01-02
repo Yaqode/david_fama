2a30d86955c4cbee9c755260e635e4c20844e7d1
Diego González Martínez <banana220833@live.com.mx>
2024-01-01 21:41:10 -0600

    Se agrega la lectura de las imagenes para marca

---
+++ src/main/java/com/market/fama/domain/UtilService.java
@@ -0,0 +1,38 @@
+package com.market.fama.domain;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+public class UtilService {
+
+    public static String leerImagenComoBase64(String rutaImagen) throws IOException {
+        File archivoImagen = new File(obtenerRutaBaseDelProyecto() + rutaImagen);
+        BufferedImage imagen = ImageIO.read(archivoImagen);
+
+        if (imagen != null) {
+            // Convertir BufferedImage a Base64
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(imagen, "jpg", baos);
+            byte[] bytes = baos.toByteArray();
+            return Base64.getEncoder().encodeToString(bytes);
+        }
+
+        return null;
+    }
+
+    public static void guardarImagenComoBase64(String base64Image, String rutaGuardar) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(obtenerRutaBaseDelProyecto() + rutaGuardar)))) {
+            writer.write(base64Image);
+        }
+    }
+
+    private static String obtenerRutaBaseDelProyecto() {
+        Path currentRelativePath = Paths.get("");
+        return currentRelativePath.toAbsolutePath().toString();
+    }
+
+}
--- src/main/java/com/market/fama/domain/service/BrandService.java
+++ src/main/java/com/market/fama/domain/service/BrandService.java
@@ -1,10 +1,12 @@
 package com.market.fama.domain.service;
 
 import com.market.fama.domain.Brand;
+import com.market.fama.domain.UtilService;
 import com.market.fama.domain.repository.BrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +15,20 @@ public class BrandService {
     @Autowired
     private BrandRepository brandRepository;
 
+    private UtilService utilService;
+
     public List<Brand> getAll() {
+        List<Brand> listBrand = brandRepository.getAll();
+        List<Brand> listBrandImg = null;
+        listBrand.forEach(brand -> {
+            try {
+                brand.setLinkImageBrand(utilService.leerImagenComoBase64("/marcas/" + brand.getLinkImageBrand()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            listBrandImg.add(brand);
+        });
+
         return brandRepository.getAll();
     }
 
